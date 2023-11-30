1b6ba2b883dc5a8f0d0069be86bdcb6a6ba6ca57
KaiYanagisawa <12730ky@gmail.com>
2023-11-30 14:59:03 +0900

    リファクタリング

--- practice/alds_1_1_a_insertion_sort.cpp
+++ practice/alds_1_1_a_insertion_sort.cpp
@@ -8,6 +8,7 @@ void insertion_sort(int N, int array[])
   {
     int v = array[i];
     int j = i - 1;
+
     while (j >= 0 && array[j] > v)
     {
       array[j + 1] = array[j];
@@ -16,9 +17,7 @@ void insertion_sort(int N, int array[])
     array[j + 1] = v;
 
     for (int i = 0; i < N; i++)
-    {
       (i == N - 1) ? cout << array[i] : cout << array[i] << " ";
-    }
     cout << endl;
   }
 }
@@ -30,9 +29,7 @@ int main()
 
   int array[1000];
   for (int i = 0; i < N; i++)
-  {
     cin >> array[i];
-  }
 
   insertion_sort(N, array);
 
