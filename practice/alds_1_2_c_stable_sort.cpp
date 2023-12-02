5af5b7f09e76d7a8aa7f1c984107d51669bbec58
KaiYanagisawa <12730ky@gmail.com>
2023-12-02 15:26:30 +0900

    bubble sort

--- practice/alds_1_2_c_stable_sort.cpp
+++ practice/alds_1_2_c_stable_sort.cpp
@@ -8,10 +8,29 @@ struct card
   int value;
 };
 
+void bubble_sort(card trump[], int N)
+{
+  for (int i = 0; i < N; i++)
+   for (int j = N - 1; j > i; j--)
+   {
+    if (trump[j - 1].value > trump[j].value)
+      swap(trump[j - 1], trump[j]);
+   }
+}
+
 int main()
 {
   int N;
   cin >> N;
 
-  card trump[36];
+  card trump[36], trump2[36];
+  for (int i = 0; i < N; i++)
+    cin >> trump[i].suit >> trump[i].value;
+
+  for (int i = 0; i < N; i++)
+    trump2[i] = trump[i];
+
+  bubble_sort(trump, N);
+  for (int i = 0; i < N; i++)
+    cout << trump[i].suit << trump[i].value << " ";
 }
